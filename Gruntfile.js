4a66c2709ab203f63e072ed18e6c30606f8701b6
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-04-30 22:13:09 -0400

    add automated tests through watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,27 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
+ 
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+ 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'src/<%= pkg.name %>.js',
-    //     dest: 'build/<%= pkg.name %>.min.js'
-    //   }
-    // },
 
-    runTests: {
-
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
     },
 
     watch: {
       scripts: {
-        files: [
-          '**/*.js'
-        ],
-        tasks: ['runTests']
+        files: ['**/*.js'],
+        tasks: ['test']
       }
     }
   });
-
-  // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task(s).
-  grunt.registerTask('default', ['runTests']);
-
+ 
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('default', 'watch');
 };
